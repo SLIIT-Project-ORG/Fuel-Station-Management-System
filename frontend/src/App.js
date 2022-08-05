fa54fe45660d20b47cd40fdd0fad11d0c7469839
Tharuka Gayashan <tharuka.gayashan.5@gmail.com>
2022-08-05 14:55:16 +0530

    project setup

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1 className="mt-5">Development in progress</h1>
+      <img src="https://gifimage.net/wp-content/uploads/2018/04/progress-gif.gif" alt="in progress" className="mt-5"/>
+
+    </div>
+  );
+}
+
+export default App;
